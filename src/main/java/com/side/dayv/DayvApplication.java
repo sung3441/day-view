eea1c62b4b610300d4ecf1ceb138b5e64be3a89d
giljin <kiljin1012@naver.com>
2023-04-13 16:22:37 +0900

    init projext

---
+++ src/main/java/com/side/dayv/DayvApplication.java
@@ -0,0 +1,13 @@
+package com.side.dayv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayvApplication.class, args);
+	}
+
+}
