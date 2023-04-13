2c9bdb11a4b586bae711e08208935d7f68cd369b
giljin <kiljin1012@naver.com>
2023-04-13 16:34:45 +0900

    test: commit test

--- src/main/java/com/side/dayv/DayvApplication.java
+++ src/main/java/com/side/dayv/DayvApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DayvApplication {
 
 	public static void main(String[] args) {
+		System.out.println("1");
 		SpringApplication.run(DayvApplication.class, args);
 	}
 
