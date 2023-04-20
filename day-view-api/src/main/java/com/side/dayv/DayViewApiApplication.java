7d28119e668501ccfb5518e2db4ca4e431125cba
chosungil <whtjddlf56@naver.com>
2023-04-20 21:39:26 +0900

    init project backend

---
+++ day-view-api/src/main/java/com/side/dayv/DayViewApiApplication.java
@@ -0,0 +1,13 @@
+package com.side.dayv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayViewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayViewApiApplication.class, args);
+	}
+
+}
---
+++ day-view-api/src/test/java/com/side/dayv/DayViewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.side.dayv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayViewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
