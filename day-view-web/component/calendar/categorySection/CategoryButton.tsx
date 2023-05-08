c265031fdb7f12c539a1ee40509b90253d3280aa
sehwa <love7620640@naver.com>
2023-05-08 21:54:32 +0900

    feat: 카테고리 버튼 기능구현

---
+++ day-view-web/component/calendar/categorySection/CategoryButton.tsx
@@ -0,0 +1,46 @@
+import { ReactNode, useState } from 'react';
+import styled, { css } from 'styled-components';
+
+interface Props {
+  children: ReactNode;
+}
+
+const CategoryButton = ({ children }: Props) => {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const toggleHandler = () => {
+    setIsSelected(!isSelected);
+  };
+
+  return (
+    <Btn isSelected={isSelected} onClick={toggleHandler}>
+      {children}
+    </Btn>
+  );
+};
+
+export default CategoryButton;
+
+const Btn = styled.button<{ isSelected: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 4px 20px;
+  width: 88px;
+  height: 30px;
+  border-radius: 20px;
+
+  ${({ theme, isSelected }) =>
+    isSelected
+      ? css`
+          color: ${theme.colors.main};
+          border: 1px solid ${theme.colors.main};
+          background: #fff;
+        `
+      : css`
+          color: #222;
+          border: none;
+          background: ${theme.colors.G_200};
+        `}
+`;
