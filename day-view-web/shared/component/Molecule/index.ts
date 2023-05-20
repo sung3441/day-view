c49f01313a8f465371f6a8ba0e991988f2b27b4f
Si-off <noismik97@gmail.com>
2023-05-20 17:15:37 +0900

    feat: ModalCreateChannel, ModalManageChannel

--- day-view-web/shared/component/Molecule/index.ts
+++ day-view-web/shared/component/Molecule/index.ts
@@ -1,4 +1,3 @@
 export { default as IconButton } from './IconButton';
 export { default as SearchBar } from './SearchBar';
 export { default as CheckBox } from './CheckBox';
-export { default as UserList } from './UserList';
