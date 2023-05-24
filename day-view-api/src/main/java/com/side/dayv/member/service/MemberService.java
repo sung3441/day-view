b963b82515e7cce9442f777d844b04b3f4e18ec8
giljin <kiljin1012@naver.com>
2023-05-24 22:03:32 +0900

    feat: 로그아웃 기능 추가

--- day-view-api/src/main/java/com/side/dayv/member/service/MemberService.java
+++ day-view-api/src/main/java/com/side/dayv/member/service/MemberService.java
@@ -2,7 +2,9 @@ package com.side.dayv.member.service;
 
 import com.side.dayv.member.entity.Member;
 import com.side.dayv.member.repository.MemberRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +15,15 @@ public class MemberService {
 
     public Member getMember(String email){
         return memberRepository.findByEmail(email);
+    }
+
+    @Transactional
+    public void removeRefreshToken(String email){
+        Member member = memberRepository.findByEmail(email);
+        if( member == null ){
+            throw new UsernameNotFoundException("not found username.");
+        }
+
+        member.changeRefreshToken(null);
     }
 }
