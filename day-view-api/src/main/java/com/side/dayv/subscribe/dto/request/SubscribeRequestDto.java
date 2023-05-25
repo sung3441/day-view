bb195dd8ee0e4c2b5878a4670bfa9bee19061485
chosungil <whtjddlf56@naver.com>
2023-05-25 10:28:42 +0900

    test: subscribeService 구독, 구독 취소 테스트

--- day-view-api/src/main/java/com/side/dayv/subscribe/dto/request/SubscribeRequestDto.java
+++ day-view-api/src/main/java/com/side/dayv/subscribe/dto/request/SubscribeRequestDto.java
@@ -5,13 +5,17 @@ import com.side.dayv.member.entity.Member;
 import com.side.dayv.subscribe.entity.Subscribe;
 import com.side.dayv.subscribe.entity.SubscribeAuth;
 import com.side.dayv.subscribe.entity.SubscribeColor;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Data
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class SubscribeRequestDto {
 
     private Long memberId;
