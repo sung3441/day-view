355e19282c5929667b964e30a261e5d98a77537c
khg <poohghg@gmail.com>
2023-05-30 22:43:59 +0900

    feat(/): 카카오 로그인 테스트

--- day-view-web/component/main/KakaoLogin.tsx
+++ day-view-web/component/main/KakaoLogin.tsx
@@ -5,17 +5,11 @@ import axios from "axios";
 const KakaoLogin = () => {
   const url = "http://localhost:8080/oauth2/authorization/kakao?redirect_uri=http://localhost:3000/auth/kakaoLogin"
 
-  async function kakaoLogin() {
-    try {
-      await axios.get(url,{withCredentials:true})
-    } catch (e){
-      console.log("e",e)
-    }
-  }
+
 
   return (
       <a href={url}>
-      <button onClick={kakaoLogin}> 카카오 로그인 로직 테스트</button>
+        <button > 카카오 로그인 로직 테스트</button>
       </a>
   )
 };
