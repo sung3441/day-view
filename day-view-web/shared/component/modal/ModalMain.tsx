b46517b00eb06e05109053ab8f3d1e7084d1a805
Si-off <noismik97@gmail.com>
2023-06-02 19:25:14 +0900

    refactor: ModalMain dim 렌더링 방식 변경

--- day-view-web/shared/component/modal/ModalMain.tsx
+++ day-view-web/shared/component/modal/ModalMain.tsx
@@ -1,29 +1,40 @@
-import { ReactNode } from 'react';
-import styled, { css, keyframes } from 'styled-components';
+import { Children, ReactNode, isValidElement } from 'react';
+import styled, { keyframes } from 'styled-components';
 import { pixelToRemUnit } from '@/shared/styles/util';
 
 interface Props {
   children?: ReactNode;
-  isDimmed?: boolean;
 }
 
-const ModalMain = ({ children, isDimmed }: Props) => {
+const ModalMain = ({ children }: Props) => {
+  const childrenArray = Children.toArray(children);
+
+  const { filteredChildren, selectedComponents } = childrenArray.reduce(
+    (result, child) => {
+      if (
+        isValidElement(child) &&
+        typeof child.type === 'function' &&
+        child.type.name === 'ModalDim'
+      ) {
+        result.selectedComponents.push(child);
+      } else {
+        result.filteredChildren.push(child);
+      }
+      return result;
+    },
+    {
+      filteredChildren: [] as ReactNode[],
+      selectedComponents: [] as ReactNode[],
+    }
+  );
+
   return (
     <S.Layout>
-      <S.Container isDimmed={isDimmed}>{children}</S.Container>
-      {isDimmed && <S.Dim />}
+      <S.Container>{filteredChildren}</S.Container>
+      {selectedComponents}
     </S.Layout>
   );
 };
-
-const dimFade = keyframes`
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 0.8;
-    }
-`;
 
 const fadeIn = keyframes`
   from {
@@ -61,38 +72,19 @@ const S = {
     z-index: 100;
   `,
 
-  Container: styled.div<{ isDimmed?: boolean }>`
+  Container: styled.div`
     display: flex;
     flex-direction: column;
     gap: 40px;
     position: relative;
 
     padding: ${pixelToRemUnit([60, 50])};
-    ${({ isDimmed }) =>
-      isDimmed
-        ? css`
-            box-shadow: none;
-          `
-        : css`
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
-          `};
 
     background-color: ${({ theme }) => theme.colors.White};
     border-radius: 11px;
     z-index: 200;
 
     animation: ${fadeIn} 0.3s ease forwards;
-  `,
-
-  Dim: styled.div`
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-
-    background-color: ${({ theme }) => theme.colors.Black};
-    animation: ${dimFade} 0.3s ease forwards;
   `,
 };
 
