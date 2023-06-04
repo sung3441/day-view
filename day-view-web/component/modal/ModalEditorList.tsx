d3478b1f9319684d8f900bd9414ffc944996b892
Si-off <noismik97@gmail.com>
2023-06-04 15:48:46 +0900

    refactor: ModalEditorList 편집자, 구독자 타입선택 추가

--- day-view-web/component/modal/ModalEditorList.tsx
+++ day-view-web/component/modal/ModalEditorList.tsx
@@ -1,24 +1,47 @@
-import { Button, UserImage } from '@/shared/component/Atom';
-import Modal from '@/shared/component/modal';
-
-import { useModal } from '@/shared/hooks';
 import { memo } from 'react';
+import styled from 'styled-components';
+import Modal from '@/shared/component/modal';
+import { getStyledThemProperty } from '@/shared/styles/util';
+import { Button, UserImage } from '@/shared/component/Atom';
 
-const ModalEditorList = () => {
-  const { closeModal } = useModal();
+interface Props {
+  type: 'EditorList' | 'SubscriberList';
+}
+
+const ModalEditorList = ({ type = 'EditorList' }: Props) => {
+  const modalConfig = {
+    EditorList: {
+      title: '편집자 목록',
+      description: '편집 권한을 해제할 수 있습니다.',
+      buttonLabel: '해제',
+    },
+    SubscriberList: {
+      title: '구독자 목록',
+      description: '편집 권한을 설정할 수 있습니다.',
+      buttonLabel: '설정',
+    },
+  };
+
+  const { title, description, buttonLabel } = modalConfig[type];
 
   return (
     <Modal>
-      <Modal.Header>
-        <Modal.Title>편집자 목록</Modal.Title>
-        <div>편집 권한을 해제할 수 있습니다.</div>
+      <Modal.Header
+        style={{
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          gap: 4,
+        }}
+      >
+        <Modal.Title>{title}</Modal.Title>
+        <S.Description>{description}</S.Description>
       </Modal.Header>
       <Modal.Body>
         <>
           <UserImage src="" />
           <div>name</div>
           <div>email</div>
-          <Button variant="accent">해제</Button>
+          <Button variant="accent">{buttonLabel}</Button>
         </>
       </Modal.Body>
       <Modal.Dim />
@@ -27,3 +50,12 @@ const ModalEditorList = () => {
 };
 
 export default memo(ModalEditorList);
+
+const Description = styled.div`
+  ${getStyledThemProperty('fonts', 'caption3')};
+  color: ${getStyledThemProperty('colors', 'G_700')};
+`;
+
+const S = {
+  Description,
+};
