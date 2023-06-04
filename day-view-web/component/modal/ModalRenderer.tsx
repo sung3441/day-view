8af6db5b8445d7b7aa589a97e90b9f08b04de7dd
Si-off <noismik97@gmail.com>
2023-06-04 15:39:03 +0900

    feat: ModalAddSchedule 기본 구조작업

--- day-view-web/component/modal/ModalRenderer.tsx
+++ day-view-web/component/modal/ModalRenderer.tsx
@@ -7,16 +7,24 @@ import {
   ModalCreateChannel,
   ModalManageChannel,
   ModalEditorList,
+  ModalAddSchedule,
 } from '@/component/modal';
 
 const modalComponents = {
   CreateCategory: ModalCreateChannel,
   ManageCategory: ModalManageChannel,
+  // @ts-ignore
   EditorList: ModalEditorList,
+  AddSchedule: ModalAddSchedule,
 } satisfies Record<string, React.MemoExoticComponent<() => React.ReactElement>>;
 
 export type ModalType = keyof typeof modalComponents;
 
+/**
+ * TODO: props 받고, 모달 닫는 애니메이션 적용할 수 있도록 recoilState, useModal 리팩토링
+ * TODO: 클릭 지점에 모달 위치시키기
+ * TODO: Dim 클릭 시 모달 닫기
+ */
 const ModalRenderer = () => {
   const modalList = useRecoilValue(modalListAtom);
 
@@ -35,6 +43,7 @@ const ModalRenderer = () => {
         modalList.map((modalType) => {
           const ModalComponent = modalComponents[modalType];
 
+          // @ts-ignore
           return <ModalComponent key={modalType} />;
         }),
         ref.current!
