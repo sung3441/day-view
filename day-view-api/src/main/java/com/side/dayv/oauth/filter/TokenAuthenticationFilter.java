0262ec297d6cc223a4829821fd4451d5319a5880
giljin <kiljin1012@gmail.com>
2023-06-06 20:40:37 +0900

    fix: 필터에서 토큰 인증 실패시 바로 에러처리

--- day-view-api/src/main/java/com/side/dayv/oauth/filter/TokenAuthenticationFilter.java
+++ day-view-api/src/main/java/com/side/dayv/oauth/filter/TokenAuthenticationFilter.java
@@ -3,6 +3,9 @@ package com.side.dayv.oauth.filter;
 import com.side.dayv.oauth.token.AuthToken;
 import com.side.dayv.oauth.token.AuthTokenProvider;
 import com.side.dayv.global.util.HeaderUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,6 +13,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -26,11 +30,22 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
         String tokenStr = HeaderUtil.getAccessToken(request);
         AuthToken token = tokenProvider.convertAuthToken(tokenStr);
 
-        if(token.validate()){
+        try{
             Authentication authentication = tokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
 
-        filterChain.doFilter(request, response);
+            filterChain.doFilter(request, response);
+        }catch (SecurityException e){
+            log.info("Invalid JWT signature");
+        }catch (MalformedJwtException e){
+            log.info("Invalid JWT token. ");
+        }catch (ExpiredJwtException e){
+            log.info("Expired JWT token.");
+        }catch (UnsupportedJwtException e){
+            log.info("Unsupported JWT token.");
+        }catch (IllegalArgumentException e){
+            log.info("JWT token compact of handler are invalid.");
+        }
+        response.setStatus(HttpStatus.FORBIDDEN.value());
     }
 }
--- day-view-api/src/main/java/com/side/dayv/oauth/token/AuthToken.java
+++ day-view-api/src/main/java/com/side/dayv/oauth/token/AuthToken.java
@@ -49,24 +49,12 @@ public class AuthToken {
     }
 
     public Claims getTokenClaims() {
-        try {
-            return Jwts.parserBuilder()
-                    .setSigningKey(key)
-                    .build()
-                    .parseClaimsJws(token)
-                    .getBody();
-        }catch (SecurityException e){
-            log.info("Invalid JWT signature");
-        }catch (MalformedJwtException e){
-            log.info("Invalid JWT token. ");
-        }catch (ExpiredJwtException e){
-            log.info("Expired JWT token.");
-        }catch (UnsupportedJwtException e){
-            log.info("Unsupported JWT token.");
-        }catch (IllegalArgumentException e){
-            log.info("JWT token compact of handler are invalid.");
-        }
-        return null;
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
     }
     public Claims getExpiredTokenClaims(){
         try {
