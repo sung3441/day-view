30bd4ee6ecfbde4e826e5f03dd91a982d53bf2a8
chosungil <whtjddlf56@naver.com>
2023-06-08 00:49:15 +0900

    refactor: ChannelController CommonResponse 타입 반환

--- day-view-api/src/main/java/com/side/dayv/channel/controller/ChannelController.java
+++ day-view-api/src/main/java/com/side/dayv/channel/controller/ChannelController.java
@@ -3,16 +3,15 @@ package com.side.dayv.channel.controller;
 import com.side.dayv.channel.dto.request.ChannelCreateDto;
 import com.side.dayv.channel.dto.request.ChannelSearchDto;
 import com.side.dayv.channel.dto.response.ChannelResponseDto;
-import com.side.dayv.channel.dto.response.ManageChannelResponseDto;
 import com.side.dayv.channel.entity.Channel;
 import com.side.dayv.channel.entity.ChannelSelectType;
 import com.side.dayv.channel.service.ChannelService;
-import com.side.dayv.global.response.ApiResponse;
+import com.side.dayv.global.response.CommonResponse;
 import com.side.dayv.oauth.entity.CustomUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,35 +26,26 @@ public class ChannelController {
     private final ChannelService channelService;
 
     @PostMapping
-    public ApiResponse<ChannelResponseDto> save(@AuthenticationPrincipal final CustomUser user,
-                                                @RequestBody final ChannelCreateDto request) {
+    public ResponseEntity<CommonResponse> save(@AuthenticationPrincipal final CustomUser user,
+                               @RequestBody final ChannelCreateDto request) {
 
         Channel saveChannel = channelService.save(user.getMemberId(), request);
 
-        return ApiResponse.success(
-                "channel",
-                new ChannelResponseDto(saveChannel, saveChannel.getCreateMember(), true, 1L)
-        );
+        return ResponseEntity.ok(new CommonResponse(new ChannelResponseDto(saveChannel, saveChannel.getCreateMember(), true, 1L)));
     }
 
     @GetMapping("/{channelSelectType}")
-    public ApiResponse<List<ManageChannelResponseDto>> findMyChannels(@AuthenticationPrincipal final CustomUser user,
+    public ResponseEntity<CommonResponse> findMyChannels(@AuthenticationPrincipal final CustomUser user,
                                                                       @PathVariable final ChannelSelectType channelSelectType) {
 
-        return ApiResponse.success(
-                channelSelectType.getKey(),
-                channelService.findMyChannels(user.getMemberId(), channelSelectType)
-        );
+        return ResponseEntity.ok(new CommonResponse(channelService.findMyChannels(user.getMemberId(), channelSelectType)));
     }
 
     @GetMapping
-    public ApiResponse<Page<ChannelResponseDto>> findChannels(@AuthenticationPrincipal final CustomUser user,
+    public ResponseEntity<CommonResponse> findChannels(@AuthenticationPrincipal final CustomUser user,
                                                               final Pageable pageable,
                                                               ChannelSearchDto search) {
 
-        return ApiResponse.success(
-                "channels",
-                channelService.findChannels(user.getMemberId(), pageable, search)
-        );
+        return ResponseEntity.ok(new CommonResponse(channelService.findChannels(user.getMemberId(), pageable, search)));
     }
 }
