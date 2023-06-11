711be7c98a0f644b1a80e75eea7d03a4dd8db6f6
giljin <kiljin1012@gmail.com>
2023-06-11 22:26:32 +0900

    feat: ErrorMessage 추가

--- day-view-api/src/main/java/com/side/dayv/global/util/ErrorMessage.java
+++ day-view-api/src/main/java/com/side/dayv/global/util/ErrorMessage.java
@@ -14,4 +14,8 @@ public class ErrorMessage {
     public static final String SUBSCRIBE_NOT_FOUND = "구독 정보를 찾을 수 없습니다.";
     public static final String BAD_REQUEST_MANAGE_UNSUBSCRIBE = "매니저 권한을 가지고 있는 구독은 취소할 수 없습니다.";
     public static final String BAD_REQUEST_PERMISSION = "다른 계정의 구독을 수정 또는 삭제할 수 없습니다.";
+
+    public static final String MODIFY_NO_PERMISSION = "수정할 권한이 없습니다";
+
+    public static final String RECORD_NOT_FOUND = "해당 일정을 찾을 수 없습니다.";
 }
