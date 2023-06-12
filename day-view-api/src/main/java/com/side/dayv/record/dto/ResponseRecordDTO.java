373227e7887ab75d4d6963b9d533626368a38e72
giljin <kiljin1012@naver.com>
2023-06-12 21:18:49 +0900

    feat: 레코드 수정

--- day-view-api/src/main/java/com/side/dayv/record/dto/ResponseRecordDTO.java
+++ day-view-api/src/main/java/com/side/dayv/record/dto/ResponseRecordDTO.java
@@ -4,6 +4,7 @@ import com.side.dayv.record.entity.Record;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
@@ -12,8 +13,11 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class ResponseRecordDTO {
 
+    private Long recordId;
     private String title;
     private String content;
+
+    private boolean complete;
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime startDate;
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
@@ -21,10 +25,12 @@ public class ResponseRecordDTO {
     private String recordImageUrl;
 
     public ResponseRecordDTO(Record record){
+        this.recordId = record.getId();
         this.title = record.getTitle();
-        this.content = record.getTitle();
+        this.content = record.getContent();
         this.startDate = record.getStartDate();
         this.endDate = record.getEndDate();
         this.recordImageUrl = record.getImageUrl();
+        this.complete = record.isComplete();
     }
 }
