b28ab8c92b6eb259c635ff8c5781e348e56ea29a
khg <poohghg@gmail.com>
2023-06-17 17:22:17 +0900

    feat(shared): 리팩토링

--- day-view-web/shared/api/index.ts
+++ day-view-web/shared/api/index.ts
@@ -1,7 +1,14 @@
 import { Client } from '@/shared/axios';
 import { UserType } from '@/shared/types/api';
 
+export const getAccessToken = async () => {
+  const res = await new Client('/api/v1/auth/refresh').get();
+  console.log('res', res);
+  return res;
+};
+
 export const getUser = async () => {
   const res = await new Client('/api/members').get<UserType>();
+  console.log('res', res);
   return res;
 };
