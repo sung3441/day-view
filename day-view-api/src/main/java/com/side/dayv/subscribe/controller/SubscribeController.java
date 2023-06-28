0302a7b28149b004becca34b762820c5524eb5b2
giljin <kiljin1012@naver.com>
2023-06-28 21:25:56 +0900

    refactor: 컨트롤러 반환 타입 변경

--- day-view-api/src/main/java/com/side/dayv/subscribe/controller/SubscribeController.java
+++ day-view-api/src/main/java/com/side/dayv/subscribe/controller/SubscribeController.java
@@ -1,11 +1,15 @@
 package com.side.dayv.subscribe.controller;
 
 import com.side.dayv.global.response.ApiResponse;
+import com.side.dayv.global.response.CommonResponse;
 import com.side.dayv.oauth.entity.CustomUser;
 import com.side.dayv.subscribe.dto.request.SubscribeUpdateDto;
+import com.side.dayv.subscribe.entity.ResponseSubscribeDTO;
+import com.side.dayv.subscribe.entity.Subscribe;
 import com.side.dayv.subscribe.service.SubscribeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,21 +22,21 @@ public class SubscribeController {
     private final SubscribeService subscribeService;
 
     @PostMapping("/channels/{channelId}/subscribes")
-    public ApiResponse subscribe(@AuthenticationPrincipal final CustomUser user, @PathVariable final Long channelId) {
-        subscribeService.subscribe(user.getMemberId(), channelId);
-        return ApiResponse.success();
+    public ResponseEntity subscribe(@AuthenticationPrincipal final CustomUser user, @PathVariable final Long channelId) {
+        ResponseSubscribeDTO subscribe = subscribeService.subscribe(user.getMemberId(), channelId);
+        return ResponseEntity.ok(new CommonResponse(subscribe));
     }
 
     @DeleteMapping("/channels/{channelId}/subscribes")
-    public ApiResponse unsubscribe(@AuthenticationPrincipal final CustomUser user, @PathVariable final Long channelId) {
+    public ResponseEntity unsubscribe(@AuthenticationPrincipal final CustomUser user, @PathVariable final Long channelId) {
         subscribeService.unsubscribe(user.getMemberId(), channelId);
-        return ApiResponse.success();
+        return ResponseEntity.ok("");
     }
 
     @PatchMapping("/subscribes/{subscribeId}")
-    public ApiResponse update(@AuthenticationPrincipal final CustomUser user,
-                              @PathVariable final Long subscribeId, @RequestBody SubscribeUpdateDto request) {
-        subscribeService.update(subscribeId, user.getMemberId(), request);
-        return ApiResponse.success();
+    public ResponseEntity update(@AuthenticationPrincipal final CustomUser user,
+                                 @PathVariable final Long subscribeId, @RequestBody SubscribeUpdateDto request) {
+        ResponseSubscribeDTO subscribe = subscribeService.update(subscribeId, user.getMemberId(), request);
+        return ResponseEntity.ok(new CommonResponse(subscribe));
     }
 }
