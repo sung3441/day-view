d6441365431c974845da95bba51f463094eb93dc
Si-off <noismik97@gmail.com>
2023-08-15 15:43:33 +0900

    feat: 구독자관리 모달 작업

---
+++ day-view-web/shared/component/Organism/Modal/ModalUserList.tsx
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+import { getStyledThemProperty } from '@/shared/styles/util';
+import { Button, UserImage } from '../../Atom';
+import { SubscribeMembersRes } from '@/shared/types/api';
+
+interface Props {
+  members: SubscribeMembersRes;
+}
+
+const ModalUserList = ({ members }: Props) => {
+  return (
+    <S.UserList>
+      {members.data.subscribers.map((member) => (
+        <S.UserItem key={''}>
+          <UserImage src={member.profileImageUrl} width={57} height={57} />
+          <div>
+            <S.Name>{member.name}</S.Name>
+            <S.Email>{member.email}</S.Email>
+          </div>
+          <Button variant={member.auth ? 'accent' : 'primary'}>설정</Button>
+        </S.UserItem>
+      ))}
+    </S.UserList>
+  );
+};
+
+export default ModalUserList;
+
+const UserList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  max-height: 380px;
+  overflow-y: scroll;
+  gap: 24px;
+
+  ::-webkit-scrollbar {
+    width: 4px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 2px;
+    background-color: #d9d9d9;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: #eaeaea;
+  }
+`;
+
+const UserItem = styled.li`
+  display: grid;
+  width: 100%;
+  grid-template-columns: 57px 1fr 90px;
+  align-items: center;
+  grid-column-gap: 21px;
+`;
+
+const Name = styled.div`
+  color: ${getStyledThemProperty('colors', 'Black')};
+  ${getStyledThemProperty('fonts', 'caption1')};
+  line-height: 150%;
+`;
+
+const Email = styled.div`
+  color: ${getStyledThemProperty('colors', 'G_700')};
+  ${getStyledThemProperty('fonts', 'caption3')};
+  line-height: 150%;
+`;
+
+const S = {
+  UserList,
+  UserItem,
+  Name,
+  Email,
+};
